02a75b60431ebf23c421e553dd3d29c9fb6e6e82
Chris White <chris.white@hover.to>
2019-10-25 11:42:02 -0600

    don't return groups of 1

--- sup_resolver.exs
+++ sup_resolver.exs
@@ -1,7 +1,8 @@
 defmodule SupResolver do
-  def solve(id_list, group_size) do
-    id_list
+  def solve(name_list, group_size) do
+    name_list
     |> Enum.shuffle()
     |> Enum.chunk_every(group_size)
+    |> Enum.reject(fn grouping -> length(grouping) == 1 end)
   end
 end
--- sup_resolver_test.exs
+++ sup_resolver_test.exs
@@ -5,11 +5,11 @@ ExUnit.configure(exclude: :pending, trace: true)
 
 defmodule SupResolverTest do
   use ExUnit.Case
+  @names ['Chris', 'Kevin', 'Adam', 'Trever', 'Randy', 'Jason', 'Nate']
 
   test "takes a list of ids and returns groupings" do
-    ids = [1, 2, 3, 4, 5, 6, 7]
     group_size = 2
-    result = SupResolver.solve(ids, group_size)
+    result = SupResolver.solve(@names, group_size)
 
     assert length(Enum.at(result, 0)) == 2
     assert length(Enum.at(result, 1)) == 2
@@ -17,16 +17,21 @@ defmodule SupResolverTest do
   end
 
   test "it returns a random result every time" do
-    ids = [1, 2, 3, 4, 5, 6, 7]
     group_size = 2
-    result1 = SupResolver.solve(ids, group_size)
-    result2 = SupResolver.solve(ids, group_size)
+    result1 = SupResolver.solve(@names, group_size)
+    result2 = SupResolver.solve(@names, group_size)
 
     assert Enum.at(result1, 0) != Enum.at(result2, 0)
   end
 
-  @tag :pending
-  test "it prevents a grouping from having size of 1"
+  test "it prevents a grouping from having size of 1" do
+    last_group =
+      @names
+      |> SupResolver.solve(2)
+      |> Enum.at(-1)
+
+    assert length(last_group) !== 1
+  end
 
   @tag :pending
   test "it loads previous groupings and returns new groups"
