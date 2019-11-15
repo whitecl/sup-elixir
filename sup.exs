ba0ea49423f152596c01ddd8dbd31246c19daf7b
Chris White <chris.white@hover.to>
2019-11-15 08:59:53 -0700

    basic help/usage in start script

--- sup.exs
+++ sup.exs
@@ -1,6 +1,13 @@
 Code.load_file("sup_resolver.exs", __DIR__)
 
 names = System.argv()
+help_triggers = ["", "-h", "--help"]
+
+if Enum.at(names, 0) in help_triggers || length(names) == 0 do
+  IO.puts("Usage: elixir sup.exs name1 name2 name3 etc")
+  System.halt(0)
+end
+
 IO.puts("Results for #{length(names)} names:")
 
 names
